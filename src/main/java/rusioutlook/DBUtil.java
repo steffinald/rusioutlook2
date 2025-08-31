f49548a154255d483b22ea58290ffcb2974a3fdc
Steffinald <inboxsteffinald@gmail.com>
2025-08-31 19:23:13 +0530

    Updated DBUtil for PostgreSQL

--- src/main/java/rusioutlook/DBUtil.java
+++ src/main/java/rusioutlook/DBUtil.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DBUtil {
 
-    private static final String db_URL = "jdbc:postgresql://db.guaybzqyicwyhmzunxmt.supabase.co:5432/postgres"; 
+    private static final String db_URL = "jdbc:postgresql://db.guaybzqyicwyhmzunxmt.supabase.co:5432/postgres?sslmode=require"; 
     private static final String USER = "postgres";  
     private static final String PASSWORD = "Rusi@2025%%";  
 
