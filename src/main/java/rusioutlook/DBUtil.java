e20aa65df19737fe5e33e8ba49c8c917c8857ea4
Steffinald <inboxsteffinald@gmail.com>
2025-08-31 21:46:21 +0530

    Updated DBUtil and WAR file for Render DB connection

--- src/main/java/rusioutlook/DBUtil.java
+++ src/main/java/rusioutlook/DBUtil.java
@@ -7,8 +7,8 @@ import java.sql.SQLException;
 public class DBUtil {
 
     private static final String db_URL = "postgresql://rusioutlookpostgre:UCOe1jN2ZNfqhsEG1ENoQBkiTLdNFlSV@dpg-d2q5q8je5dus73bml9vg-a.oregon-postgres.render.com/rusioutlook_database?sslmode=require"; 
-    private static final String USER = "postgres";  
-    private static final String PASSWORD = "Rusi@2025%%";  
+    private static final String USER = "rusioutlookpostgre";  
+    private static final String PASSWORD = "UCOe1jN2ZNfqhsEG1ENoQBkiTLdNFlSV";  
 
     public static Connection getConnection() throws SQLException {
     	try {
